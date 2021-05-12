a6726945806e7f0c64995ea11e47da38505ca688
mhupalo <mhupalo@rubiconproject.com>
2021-05-12 13:29:26 +0300

    Remove target dir

--- target/classes/main/groovy/resources/db/clear_tables.sql
+++
@@ -1,9 +0,0 @@
-SET SQL_SAFE_UPDATES = 0;
-delete from drugs_ingredients;
-delete from drugs;
-delete from ingredients_group_belonging;
-delete from ingredient_groups;
-delete from patients_ingredients;
-delete from patients;
-delete from ingredients;
-delete from tolerance_finder;
